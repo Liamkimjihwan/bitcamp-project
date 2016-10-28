65840b67e8ffe95b85bc91918bc4d0da9bb9f4d2
Liamkimjihwan <wwwwwwlghskdlekd@naver.com>
2016-10-28 10:57:53 +0900

    .

--- src/bitcamp/java89/ems/EduApp.java
+++ src/bitcamp/java89/ems/EduApp.java
@@ -5,44 +5,63 @@ public class EduApp {
     System.out.println("비트캠프 관리 시스템에 오신걸 환영합니다.");
 
 
-  //1) 사용자로부터 값을 입력받기 위해 사용할 입력도구 준비!
+    // 여러명의 강사 정보를 입력하기 위한 레퍼런스 배열을 만든다.
+    Teacher[] teachers = new Teacher[100]; // teacher 100개를 담을 레퍼런스
+    // 배열로 데이터를 다루면 확장성이 없음..
+   int length = 0; // 레퍼런스 배열 (Teacher[] teacher)에 몇명의 강사 정보를 저장했는지 개수를 지정한다.
+          // 레퍼런스 배열에 몇 개의 Teacher 인스턴스가 들어있는지 그 개수를 보관한다.
+
   Scanner keyScan = new Scanner(System.in);
 
-   Teacher teacher = new Teacher(); // 빨간 teacher 인스턴스 준비
-  System.out.print("이름(예:홍길동)? ");
-  teacher.name = keyScan.nextLine();
+  // 반복해서 입력 받는다.
+ // 주소 없음을 의미한다. Teacher teacher = null;
+  while (length < teachers.length) {
+    Teacher teacher = new Teacher(); // 반복문이 돌때마다 새로 생성됨.
+     System.out.print("이름(예:홍길동)? ");
+      teacher.name = keyScan.nextLine();
 
-  System.out.print("성별? ");
-  teacher.gender = keyScan.nextLine();
+     System.out.print("성별? ");
+      teacher.gender = keyScan.nextLine();
 
-  System.out.print("나이? ");
-  teacher.age = Integer.parseInt(keyScan.nextLine());
+     System.out.print("나이? ");
+      teacher.age = Integer.parseInt(keyScan.nextLine());
 
-  System.out.print("경력(예:20년차)? ");
-  teacher.career = keyScan.nextLine();
+     System.out.print("경력(예:20년차)? ");
+      teacher.career = keyScan.nextLine();
 
-  System.out.print("능력? ");
-  teacher.skill = keyScan.nextLine();
+     System.out.print("능력? ");
+      teacher.skill = keyScan.nextLine();
 
-  System.out.print("전공? ");
-  teacher.major = keyScan.nextLine();
+     System.out.print("전공? ");
+      teacher.major = keyScan.nextLine();
 
-  System.out.print("이메일(예:hong@test.com)? ");
-  teacher.email = keyScan.nextLine();
+     System.out.print("이메일(예:hong@test.com)? ");
+      teacher.email = keyScan.nextLine();
 
-  System.out.print("전화번호(예:010-1111-1111)? ");
-  teacher.tel = keyScan.nextLine();
+     System.out.print("전화번호(예:010-1111-1111)? ");
+      teacher.tel = keyScan.nextLine();
 
-  System.out.printf("이름: %s\n", teacher.name);
-  System.out.printf("성별: %s\n", teacher.gender);
-  System.out.printf("나이: %d\n", teacher.age);
-  System.out.printf("경력: %s\n", teacher.career);
-  System.out.printf("능력: %s\n", teacher.skill);
-  System.out.printf("전공: %s\n", teacher.major);
-  System.out.printf("이메일: %s\n", teacher.email);
-  System.out.printf("전화번호: %s\n", teacher.tel);
+    teachers[length++] = teacher; // length 현재 값을 두는 것. 0은 변하지않음.
+    System.out.print("계속 입력하시겠습니까(y/n)?");
+    if (!keyScan.nextLine().equals("y")) // 키스캔에 입력된 값이 y와 같지않다면 if문 실행
+    break;
+  }
+   printTeacherList(teachers, length);
+  }
 
-
+static void printTeacherList(Teacher[] teachers, int length) {
+  for (int i = 0; i < length; i++) {
+    Teacher teacher = teachers[i];
+    System.out.printf(" %s,%s,%d,%s,%s,%s,%s,%s\n",
+    teacher.name,
+    teacher.gender,
+    teacher.age,
+    teacher.career,
+    teacher.skill,
+    teacher.major,
+    teacher.email,
+    teacher.tel);
+  }
 
   }
 }
